5f6e19a2710d7666380b6f221ef318c2e786c0d0
Pablo Cid <pabloagronomo@gmail.com>
2018-10-22 14:51:50 -0300

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+
+app.get('/', (req, res) => {
+    res.send('An alligator approaches!');
+});
+
+app.get('/tomafoto', cors, function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    var args = ["--nopreview", "--timeout", "1", "-o", "-"];
+
+    var keys = Object.keys(req.query);
+    console.log(keys);
+
+    if (keys.length) {
+        for (var i = 0; i < keys.length; i++) {
+            var el = keys[i];
+            if (el === 'resize') { continue; }
+            args.push('--' + el);
+            if (req.query[el] !== '') {
+                args.push(req.query[el]);
+            }
+        }
+    }
+
+    console.log(args);
+
+    var still = spawn('raspistill', args);
+
+    if (req.query.resize) {
+        console.log('resizing ...');
+        var convert = spawn('convert', ['-', '-resize', req.query.resize, '-']);
+        still.stdout.pipe(convert.stdin);
+        convert.stdout.pipe(res);
+    } else {
+        console.log(' no resizing ...');
+        still.stdout.pipe(res);
+    }
+
+});
+
+app.listen(3000, () => console.log('Gator app listening on port 3000!'));
